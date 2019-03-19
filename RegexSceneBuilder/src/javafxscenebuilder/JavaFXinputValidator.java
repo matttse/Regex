7aeba27b41810a36d90dc34e20050d74d6e2ef10
Matthew D Tse <tsemd@bu.edu>
2019-03-19 18:37:22 -0400

    Initial commit

---
+++ RegexSceneBuilder/src/javafxscenebuilder/JavaFXinputValidator.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxscenebuilder;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author tsemd
+ */
+public class JavaFXinputValidator extends Application {
+
+    public JavaFXinputValidator() {
+        ObservableList<String> availableChoices; 
+        availableChoices = FXCollections.observableArrayList("apples", "oranges");
+    }
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
